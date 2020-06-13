71e104e2a19e556b5e35cd37b47cb67b55784578
Иван Зуев <ivan78.ru@yandex.ru>
2020-06-13 10:53:34 +0300

    g1 done

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -8,4 +8,20 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+    if len(container) == 1:
+        return container
+    else:
+        mid = len(container) // 2
+        first_mid = sort(container[:mid])
+        second_mid = sort(container[mid:])
+        sorted_list = []
+        while first_mid and second_mid:
+            if first_mid[0] > second_mid[0]:
+                sorted_list.append(second_mid.pop(0))
+            else:
+                sorted_list.append(first_mid.pop(0))
+        if first_mid:
+            sorted_list += first_mid
+        elif second_mid:
+            sorted_list += second_mid
+        return sorted_list
